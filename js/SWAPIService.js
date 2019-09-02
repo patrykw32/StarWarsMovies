7071e612b5523f55cca40a4394f08a8786556e21
patrykw33 <patryk.watras@wp.pl>
2019-09-03 00:19:25 +0200

    Get movies option added

---
+++ js/SWAPIService.js
@@ -0,0 +1,18 @@
+
+//Get movies and push into array
+async function getFilms(){
+    const movies = [];
+    let url = "https://swapi.co/api/films/";
+    while(url !== null){
+        const response = await getResponse(url);
+        movies.push(...response.results);
+    }
+    return movies;
+}
+// Get single movie
+async function getFilm(url){
+    let movie = await getResponse(url);
+    return movie; 
+}
+
+
